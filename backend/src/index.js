39d1b2da2ccbd21ff603b809fc035798a1d3250d
Hasmukh1029 <hasmukh1029@gmail.com>
2025-11-19 03:50:05 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,4 +9,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/categories", require("./routes/categories"));
 
-app.listen(4000, () => console.log("Backend running on port 4000 🚀"));
+//app.listen(4000, () => console.log("Backend running on port 4000 🚀"));
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log("Server running on port", port));
